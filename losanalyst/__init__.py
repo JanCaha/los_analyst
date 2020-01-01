3c411e0d49174bc86395657784fa68c73e9b696b
Jan Caha <jan.caha@outlook.com>
2020-01-01 23:19:39 +0100

    update main package __init__.py

--- losanalyst/__init__.py
+++ losanalyst/__init__.py
@@ -2,6 +2,6 @@
 Main module that provides access to the most important functions of the package.
 """
 
-from .create_los import *
-from .analyze_los import *
+from .create_los import create_local_los, create_global_los, create_no_target_los
+from .analyze_los import analyze_local_los, analyze_global_los, analyze_no_target_los
 from .extract_horizons import *
