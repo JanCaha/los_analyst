785d604e2d0bfe82e4fcd0b135dc7e09593734cc
Jan Caha <jan.caha@outlook.com>
2020-01-03 18:07:59 +0100

    update package documentation

--- losanalyst/__init__.py
+++ losanalyst/__init__.py
@@ -1,5 +1,19 @@
 """
 Main module that provides access to the most important functions of the package.
+
+Functions :
+
+* `losanalyst.tools.create_los.create_local_los()`
+
+* `losanalyst.tools.create_los.create_global_los()`
+
+* `losanalyst.tools.create_los.create_no_target_los()`
+
+* `losanalyst.tools.analyze_los.analyze_local_los()`
+
+* `losanalyst.tools.analyze_los.analyze_global_los()`
+
+* `losanalyst.tools.analyze_los.analyze_no_target_los()`
 """
 
 from losanalyst.tools.create_los import create_local_los, create_global_los, create_no_target_los
